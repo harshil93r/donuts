79738903d4b81e5756cebd339b667a65e20a1199
Rohit Agrawal <rohit.agrawal@innovaccer.com>
2018-11-17 15:23:31 +0530

    updated

--- patient/urls.py
+++ patient/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignUp, OTPVerify
+from .views import SignUp, OTPVerify, Login
 
 urlpatterns = [
     path('/signup', SignUp.as_view()),
     path('/otp_verify', OTPVerify.as_view()),
+    path('/login', Login.as_view())
 ]

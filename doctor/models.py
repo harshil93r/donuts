782512c2641d30f2cbdb6f3d2ff7c49d1b427d02
KamaleshTangudu-Innovaccer <kamalesh.tangudu@innovaccer.com>
2018-11-17 13:17:32 +0530

    update

--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+	ssn = models.CharField(max_length=10, null=True)
+	pcpId = models.CharField(max_length=10, null=True)
+	insuranceNo = models.CharField(max_length=10, null=True)
+	credtardNo = models.CharField(max_length=16, null=True)

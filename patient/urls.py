aa7bae08f7502442a15034f27f0f8a9c3618ea67
Harshil Rastogi <harshil9968@gmail.com>
2018-11-18 03:52:55 +0530

    update

--- patient/urls.py
+++ patient/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .auth_views import SignUp, OTPVerify, Login, Me, DoctorList, DoctorRequest, EndChat
-from .chat import Message, Attachment, Inbox
+from .chat import Message, Attachment, Inbox, Vid
 
 urlpatterns = [
     path('signup', SignUp.as_view()),
@@ -9,9 +9,10 @@ urlpatterns = [
     path('doctor_list', DoctorList.as_view()),
     path('me', Me.as_view()),
     path('doctor_request', DoctorRequest.as_view()),
-    path('chat/send_text',Message.as_view()),
-    path('chat/send_file',Attachment.as_view()),
-    path('inbox',Inbox.as_view()),
-    path('chat/messages',Message.as_view()),
-    path('endchat',EndChat.as_view())
+    path('chat/send_text', Message.as_view()),
+    path('chat/send_file', Attachment.as_view()),
+    path('inbox', Inbox.as_view()),
+    path('chat/messages', Message.as_view()),
+    path('endchat', EndChat.as_view()),
+    path('vid', Vid.as_view())
 ]
